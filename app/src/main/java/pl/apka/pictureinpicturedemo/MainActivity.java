8c01e189ea35723fc5b79fa2f8439ddcff416c68
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-02-21 19:24:12 +0100

    button funcionalities added

--- app/src/main/java/pl/apka/pictureinpicturedemo/MainActivity.java
+++ app/src/main/java/pl/apka/pictureinpicturedemo/MainActivity.java
@@ -1,8 +1,11 @@
 package pl.apka.pictureinpicturedemo;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onPictureInPictureModeChanged(boolean isInPictureInPictureMode, Configuration newConfig) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig);
+
+        TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
+
+        if (isInPictureInPictureMode){
+            //going to picture in picture
+            button.setVisibility(View.INVISIBLE);
+            getSupportActionBar().hide();
+            textView.setText("3,45$");
+        }else{
+            //going out from pip
+        }
+
     }
 }
