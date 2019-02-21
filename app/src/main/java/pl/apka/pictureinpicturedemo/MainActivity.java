2f485688363dd135642e05c93018986438297af6
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-02-21 19:12:48 +0100

    pip mode

--- app/src/main/java/pl/apka/pictureinpicturedemo/MainActivity.java
+++ app/src/main/java/pl/apka/pictureinpicturedemo/MainActivity.java
@@ -1,9 +1,14 @@
 package pl.apka.pictureinpicturedemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void pipMode(View view){
+        enterPictureInPictureMode();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
